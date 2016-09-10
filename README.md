67e88ebb585b69a4410cadfe9dd810e8ad6dc1a4
Michael Coon <mac718@gmail.com>
2016-09-10 12:48:18 -0400

    Added stock_picker.rb

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Ruby_Building_Blocks
+# Projects: Ruby Building Blocks
+Ruby Programming: http://www.theodinproject.com/ruby-programming/building-blocks?ref=lc-pb
---
+++ stock_picker.rb
@@ -0,0 +1,25 @@
+def stock_picker(prices)
+	best_days = []
+	if prices.index(prices.min) < prices.index(prices.max)
+    	best_days = [prices.index(prices.min), prices.index(prices.max)]
+    else
+    	sell_day = []
+    	buy_day = []
+    	prices_reverse = prices.reverse
+    	difference = 0
+    	prices.each do |price|
+    		prices.each do |day|
+    			if day - price > difference && prices.index(day) > prices.index(price)
+    				difference = day - price
+    				sell_day = prices.index(day)
+    				buy_day = prices.index(price)
+    			end
+    		end
+    	end
+    		best_days = [buy_day, sell_day]
+    				
+  	end
+  	best_days
+end
+
+puts stock_picker([17,3,6,9,15,8,6,1,10])
